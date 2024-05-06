5a3bc16a14ef6707e46e0c49d6dc520b9a281412
Gilad Kupfer <giladkupfer@gmail.com>
2024-05-06 21:49:02 +0300

    added new Requests and Responses

--- Trivia/Trivia/Structures.h
+++ Trivia/Trivia/Structures.h
@@ -5,47 +5,106 @@
 #include <ctime>
 #include "Constants.h"
 
+struct RoomData;
 class IRequestHandler;
 #include "IRequestHandler.h"
 
 using std::string;
 using std::vector;
 
+typedef unsigned int uint;
+
 
 // ******************* REQUESTS STRUCTURES *******************
 struct LoginRequest
 {
-	string username;	// username
-	string password;	// password
+	string username;
+	string password;
 };
 
 
 struct SignupRequest
 {
-	string username;	// username
-	string password;	// password
-	string email;		// mail
+	string username;
+	string password;
+	string mail;
 };
 
+
+struct GetPlayersInRoomRequest
+{
+	uint roomID;
+};
+
+struct JoinRoomRequest
+{
+	uint roomID;
+};
+
+struct CreateRoomRequest
+{
+	string roomName;
+	uint maxPlayers;
+	uint questionCount;
+	uint answerTimeout;
+};
 
 
 
 // ******************* RESPONSE STRUCTURES *******************
 struct LoginResponse
 {
-	unsigned int status = 1;
+	uint status = 1;
 };
 
 
 struct SignupResponse
 {
-	unsigned int status = 1;
+	uint status = 1;
 };
 
 
 struct ErrorResponse
 {
 	string message = "ERROR";
+};
+
+struct LogoutResponse
+{
+	uint status = 1;
+};
+
+struct GetRoomResponse
+{
+	uint status = 1;
+	vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse
+{
+	vector<string> players;
+};
+
+struct GetHighScoreResponse
+{
+	uint status = 1;
+	vector<string> statistics;
+};
+
+struct GetPersonalStatsResponse
+{
+	uint status = 1;
+	vector<string> statistics;
+};
+
+struct JoinRoomResponse
+{
+	uint status = 1;
+};
+
+struct CreateRoomResponse
+{
+	uint status = 1;
 };
 
 
@@ -70,7 +129,6 @@ struct RequestResult
 };
 
 
-typedef unsigned int uint;
 struct RoomData
 {
 	uint id;
