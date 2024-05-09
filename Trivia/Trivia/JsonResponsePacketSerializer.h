017b8b1035ff8d2fe8f2c4f50e2c58001e92097a
Itay Shalev <itayshallev@gmail.com>
2024-05-09 18:27:37 +0300

    Updated 'JsonResponsePacketSerializer' class header file

--- Trivia/Trivia/JsonResponsePacketSerializer.h
+++ Trivia/Trivia/JsonResponsePacketSerializer.h
@@ -13,4 +13,12 @@ public:
 	static Buffer serializeResponse(const ErrorResponse& errorResponse);
 	static Buffer serializeResponse(const LoginResponse& loginResponse);
 	static Buffer serializeResponse(const SignupResponse& signupResponse);
+
+	static Buffer serializeResponse(const LogoutResponse& signupResponse);
+	static Buffer serializeResponse(const GetRoomResponse& signupResponse);
+	static Buffer serializeResponse(const GetPlayersInRoomResponse& signupResponse);
+	static Buffer serializeResponse(const JoinRoomResponse& signupResponse);
+	static Buffer serializeResponse(const CreateRoomResponse& signupResponse);
+	static Buffer serializeResponse(const GetHighScoreResponse& signupResponse);
+	static Buffer serializeResponse(const GetPersonalStatsResponse& signupResponse);
 };
