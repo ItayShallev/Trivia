7a210bcf4fe057ea841475e0ae99339d094afac2
Itay Shalev <itayshallev@gmail.com>
2024-06-17 23:56:58 +0300

    Fixed a bug that crashed the client when the user tried to login after a signout

--- Trivia/Trivia/LoginManager.h
+++ Trivia/Trivia/LoginManager.h
@@ -10,14 +10,14 @@ using std::vector;
 class LoginManager
 {
 public:
-	LoginManager(IDatabase* database, const vector<LoggedUser>& loggedUsers);
+	LoginManager(IDatabase* database, const vector<std::shared_ptr<LoggedUser>>& loggedUsers);
 
-	bool signup(const string& username, const string& password, const string& mail) const;
+	bool signup(const string& username, const string& password, const string& mail);
 	bool login(const string& username, const string& password);
 	bool logout(const string& username);
 	bool doesUserExist(const string& username);
 
 private:
 	IDatabase* m_database;
-	vector<LoggedUser> m_loggedUsers;
+	vector<std::shared_ptr<LoggedUser>> m_loggedUsers;
 };
